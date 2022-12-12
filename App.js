97d3742cd471e40cfe059ddfed3ae8af9e7aa02e
yuven2511 <120372522+yuven2511@users.noreply.github.com>
2022-12-12 15:24:01 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import College from "./college";
+import Welcome from "./Components/Welcome";
+
+function App(){
+
+return(
+  <div>
+
+    <College></College>
+.
+
+
+  </div>
+)
+
+
+}
+export default App;
+
+
---
+++ college.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+import './style1.css';
+
+
+export default class College extends Component {
+  render() {
+    return (
+      <div>
+        <div className='container-top'>
+                
+                <h2>SRI ACHIEVERS INSTITUTE OF ENGINEERING AND  TECHNOLOGY </h2>
+                <h2>AN AUTONOMOUS INSTITUTION </h2>
+            </div>
+            <div className="container-mid">
+                <button className="btn">Home</button>
+                <button className="btn1">Technical Events</button>
+                <button className="btn">Quiz</button>
+                <button className="btn">Workshop</button>
+                
+              </div>
+                <h2 id="p1">RULES AND REGULATION</h2>
+              <div className='dddd'>
+                <h3 id="p2">1.Maximum number of participants in a Team is 3.</h3>                                                     
+                <h3 id="p3">2.Abstract should not exceed more than 250 words and Paper should not exceed 15 pages.</h3>
+                <h3 id="p4">3.The subject of the mail with the attached abstract and later, the final paper should be the domain and sub-category under which the </h3>
+                <h3 id="p5">paper is categorized.</h3>
+                <h3 id="p6">4.Bring your college I-Card & receipt on event day.</h3>
+                <h3 id="p7">5.Kindly bring your PowerPoint presentation on a CD or on a pen-drive</h3>
+                <h3 id="p8">6.Soft-copies of the submitted paper and PowerPoint presentations must be in the .docx format.</h3>
+                <h3 id="p9">7.Kindly Mail your abstract, paper and ppt to isopteronz2k20@gmail.com. Difficulties will be addressed through the same email-id.</h3>
+                <h3 id="p10">8.The teams will get 10 minutes for presentation and followed by question and answer session.</h3>
+                <h3 id="p11">9.Persons from different institutions can be a part of the same team. However, one person may not be a part of multiple teams for the </h3>
+                <h3 id="p12">same event.</h3>
+              </div>
+            </div>
+          
+    )
+    }
+  }
